e31165747fceb7820879bf0e78e02c94efa05faa
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-17 20:05:20 -0500

    Fiexed some issues with the server

--- api/server.js
+++ api/server.js
@@ -3,14 +3,21 @@ const helmet = require("helmet");
 const cors = require("cors");
 const morgan = require("morgan");
 
+const authenticate = require("../auth/authenticate-middleware");
+const authRouter = require("../auth/auth-router.js");
+const listingsRouter = require("../listings/listing-router");
+
 const server = express();
 
 const setting = [helmet(), cors(), morgan("dev"), express.json()];
 
 server.use(setting);
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'running' });
+server.use("/api/auth", authRouter);
+server.use("/api", authenticate, listingsRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "running" });
 });
 
 module.exports = server;
