cc4177fbe4d4a38692b47e9549ffaf87d07ca5ee
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-18 19:52:12 -0500

    Fixed issues with seed

--- database/seeds/001_users.js
+++ database/seeds/001_users.js
@@ -7,7 +7,6 @@ const hashPassword = password => {
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("users")
-    .truncate()
     .then(function() {
       // Inserts seed entries
       return knex("users").insert([
