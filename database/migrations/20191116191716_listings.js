5608bdf7187cd16317eea927d9cfabd36e4ea31b
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-21 13:38:39 -0500

    Create new tables to reflect the changes done my the data science team

--- database/migrations/20191116191716_listings.js
+++ database/migrations/20191116191716_listings.js
@@ -9,8 +9,13 @@ exports.up = function(knex) {
     tbl.float("bathrooms").notNullable();
     tbl.string("room_type", 255).notNullable();
     tbl.boolean("wifi").notNullable();
+    tbl.boolean("tv").notNullable();
+    tbl.boolean("Laptop_friendly_workspace").notNullable();
+    tbl.boolean("family_kid_friendly").notNullable();
+    tbl.boolean("smoking_allowed").notNullable();
     tbl.integer("minimum_nights").notNullable();
-    tbl.string("description", 255).notNullable();
+    tbl.integer("extra_people").notNullable();
+    tbl.integer("cleaning_fee").notNullable();
     tbl.integer("optimal_price").notNullable();
     tbl
       .integer("users_id")
