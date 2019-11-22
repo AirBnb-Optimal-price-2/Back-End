0355ea67c99edf9c9ffc9619c194ae287543471f
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2019-11-22 14:19:45 -0500

    Completed changes

--- database/seeds/001_users.js
+++ database/seeds/001_users.js
@@ -6,7 +6,7 @@ const hashPassword = password => {
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("users").truncate()
+  return knex("users")
     .then(function() {
       // Inserts seed entries
       return knex("users").insert([
--- database/seeds/002_listings.js
+++ database/seeds/002_listings.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("listings").truncate()
+  return knex("listings")
     .then(function() {
       // Inserts seed entries
       return knex("listings").insert([
